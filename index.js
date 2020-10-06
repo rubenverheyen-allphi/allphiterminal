8635eee09a35856687f56cea658202410fc8613b
Ruben Verheyen <RubenV@svhg.be>
2020-10-06 15:13:14 +0200

    allphi-command

--- index.js
+++ index.js
@@ -1,4 +1,60 @@
-var myShell = new TermlyPrompt('#terminal-container', {});
-//var myShell = new TermlyPrompt('#container', {
-    /* options object */
-//  });
+var customCommands = {
+    allphi: {
+        name: 'allphi',
+        man: 'todo: manual for AllPhi',
+        fn: function allphi(ARGV) {
+          if (!ARGV || !ARGV['_'].length) {
+            return this.man;
+          }
+
+          let input = ARGV['_'];
+
+          if (input[0].toLowerCase().trim() === 'ceo') {
+            return 'Roel Vaneerdewegh';
+          }
+
+          if (input[0].toLowerCase().trim() === 'whois') {
+            return '"AllPhi is een onafhankelijk consultancy bedrijf gespecialiseerd in software ontwikkeling met een duidelijke focus op technologie (Microsoft .NET)."';
+          }
+
+          if (input[0].toLowerCase().trim() === 'mission') {
+            return '\"AllPhi enables the next generation software developers who will change and build the world of tomorrow in which we live, love and work.\"'
+          }
+
+          if (input[0].toLowerCase().trim() === 'logo') {
+            return logo();
+          }
+
+          return `No action found for "allphi ${input[0]}"`;
+        }
+    }
+  }
+
+var myShell = new TermlyPrompt('#terminal-container', {env: {USER: 'dev ', HOSTNAME: ' &phi;'}});
+customCommands.help = myShell.ShellCommands.help;
+customCommands.man = myShell.ShellCommands.man;
+myShell.ShellCommands = myShell.registerCommands(myShell, customCommands);
+
+function logo() { 
+  return `@@@@@@@@@@@@@@@@@@@@@@(((((//@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@((((((((((((((((((((((/@@@@@@@@@@@@@
+@@@@@@@@@@(((((((((((((((((((((((((((((((@@@@@@@@@
+@@@@@@@(((((((((((.   ((((       /(((((((((/@@@@@@
+@@@@@((((((((((    (((((    ((/     ,((((((((/@@@@
+@@@*(((((((((    /((((((   ((((((     (((((((((@@@
+@@((((((((((     (((((((   (((((((     (((((((((,@
+@/((((((((((    *(((((((   ((((((((    ((((((((((@
+@((((((((((/    ,(((((((   ((((((((    (((((((((((
+/(((((((((((     (((((((   ((((((((    (((((((((((
+(((((((((((((    ,((((((   (((((((    ((((((((((((
+/(((((((((((((     (((((   ((((((   *(((((((((((((
+@((((((((((((((((     ((   ((     ((((((((((((((((
+@/((((((((((((((((((((((   ((((((((((((((((((((((@
+@@/(((((((((((((((((((((  .(((((((((((((((((((((@@
+@@@@((((((((((((((((((((  .((((((((((((((((((((@@@
+@@@@@/((((((((((((((((((   (((((((((((((((((( @@@@
+@@@@@@@/((((((((((((((((   ((((((((((((((((@@@@@@@
+@@@@@@@@@@/((((((((((((((((((((((((((((( @@@@@@@@@
+@@@@@@@@@@@@@@/(((((((((((((((((((((@@@@@@@@@@@@@@  
+`;
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+# AllPhi terminal
+
+## Goal of the project
+
+The goal of this project is to keep in touch with web development. This is an exercise, not a real project.
+
+## Resources
+
+- the editor: [VSCode](https://code.visualstudio.com/)
+- the library: [Termly.js](https://kirkhammetz.github.io/termly.js/#/)
+- the httpserver with live reload: [Live Server](https://github.com/tapio/live-server#readme)
+- the company: [AllPhi](https://www.allphi.eu/)
